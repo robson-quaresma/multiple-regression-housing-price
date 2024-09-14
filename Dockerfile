5145667554613b46befed3b5852419ca9e25a15a
Robson Quaresma <robquaresma@gmail.com>
2024-09-14 18:03:17 -0300

    API

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+#
+FROM python:3.12
+
+#
+WORKDIR /code
+
+#
+COPY ./requirements.txt /code/requirements.txt
+
+#
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+#
+COPY main.py /code/app
+COPY model.pkl /code/app
+
+#
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import pickle
+import pandas as pd
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict(data: dict):
+    df = pd.DataFrame(data, index=[0])
+
+    prediction = model.predict(df)
+
+    return {'prediction': prediction[0]}
